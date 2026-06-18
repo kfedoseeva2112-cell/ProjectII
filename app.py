7585ed465efb5f11ace673322439e2629dff6577
kfedoseeva2112-cell <kfedoseeva2112@gmail.com>
2026-06-18 09:37:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-st.markdown("#### 💄 Макияж")
+import streamlit as st
+from PIL import Image
+from api_analyzer import analyze_face
+from matcher import get_recommendationsst.markdown("#### 💄 Макияж")
 makeup_list = rec.get("makeup")
 if makeup_list and len(makeup_list) > 0:
     st.markdown(f"""
